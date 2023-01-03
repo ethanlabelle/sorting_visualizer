334de707f6c4e67017ec427468eb9ff1a8274e04
Ethan LaBelle <ethan.labelle01@gmail.com>
2023-01-03 03:12:59 -0500

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sorting_visualizer
+
+Currently, this code performs three heapsorts over a board of random pixels, once per color channel.
--- sorter.py
+++ sorter.py
@@ -1,47 +1,17 @@
 from graphics import *
 import random
+import cProfile
+import math
 
-arr_width = 1000
-win = GraphWin(width=arr_width, height=arr_width)
-
-input('''
-Press Enter to Begin
-''')
-
-#print('''Choose sorting algorithm:''')
-#print('''Enter:''')
-#print('''\t(a) heap sort''')
-#input('''\t(b) bubble sort''')
-    
-    # algorithm = input('''\t(quit) quit\n''')
-    #if algorithm == "quit":
-    #    break
-    # print(algorithm)
-def selection_sort(array, dim):
-    for i in range(len(array)):
-        min_ind = i
-        for j in range(i+1, len(array)):
-            if array[j][dim] < array[min_ind][dim]:
-                 min_ind = j
-        if min_ind != i:
-            array[min_ind], array[i] = array[i], array[min_ind]
-            p1 = Point(min_ind//50 * 10, min_ind%50 * 10)
-            p2 = Point(min_ind//50 * 10 + 10, min_ind%50 * 10 + 10)
-            rect = Rectangle(p1, p2)
-            rect.setFill(color_rgb(*array[min_ind]))
-            rect.setOutline(color_rgb(*array[min_ind]))
-            rect.draw(win)
-            
-            p1 = Point(i//50 * 10, i%50 * 10)
-            p2 = Point(i//50 * 10 + 10, i%50 * 10 + 10)
-            rect = Rectangle(p1, p2)
-            rect.setFill(color_rgb(*array[i]))
-            rect.setOutline(color_rgb(*array[i]))
-            rect.draw(win)
-    return array
+window_width = 500
+n = 100
+win = GraphWin(width=window_width, height=window_width)
+array = []
+board = []
+row_size = int(math.sqrt(n))
+square_width = int(window_width/row_size)
 
 class Heap:
-
     def __init__(self):
         self.size = 0
         self.heap = []
@@ -71,20 +41,10 @@ class Heap:
             largest = r
         if largest != i:
             self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
-            p1 = Point(largest//10 * 100, largest%10 * 100)
-            p2 = Point(largest//10 * 100 + 100, largest%10* 100 + 100)
-            rect = Rectangle(p1, p2)
-            rect.setFill(color_rgb(*self.heap[largest]))
-            rect.setOutline(color_rgb(*self.heap[largest]))
-            rect.draw(win)
-            
-            p1 = Point(i//10 * 100, i%10 * 100)
-            p2 = Point(i//10 * 100 + 100, i%10 * 100 + 100)
-            rect = Rectangle(p1, p2)
-            rect.setFill(color_rgb(*self.heap[i]))
-            rect.setOutline(color_rgb(*self.heap[i]))
-            rect.draw(win)
-#            print(self.heap)
+            board[largest].setFill(color_rgb(*self.heap[largest]))
+            board[largest].setOutline(color_rgb(*self.heap[largest]))
+            board[i].setFill(color_rgb(*self.heap[i]))
+            board[i].setOutline(color_rgb(*self.heap[i]))
             self.max_heapify(largest, dim)
         return
     
@@ -98,63 +58,48 @@ class Heap:
         return max_elm
 
     def build_max_heap(self, array, dim):
-#        print("heap.build_max_heap(array)")
         self.size = len(array)
         self.heap = array
         for i in range(int(self.size/2), -1, -1):
             self.max_heapify(i, dim)
         print(f"MAX HEAP FINISHED dim {dim}") 
-#        print(self.heap)
-#        print()
+
 def heapsort(array, dim):
     heap = Heap()
     heap.build_max_heap(array, dim)
     for i in range(len(array) - 1, 0, -1):
+        # remove max element
         array[0], array[i] = array[i], array[0]
-        p1 = Point(0, 0)
-        p2 = Point(100, 100)
-        rect = Rectangle(p1, p2)
-        rect.setFill(color_rgb(*array[0]))
-        rect.setOutline(color_rgb(*array[0]))
-        rect.draw(win)
-            
-        p1 = Point(i//10 * 100, i%10 * 100)
-        p2 = Point(i//10 * 100 + 100, i%10 * 100 + 100)
-        rect = Rectangle(p1, p2)
-        rect.setFill(color_rgb(*array[i]))
-        rect.setOutline(color_rgb(*array[i]))
-        rect.draw(win)
         heap.size -= 1
+        # update tiles
+        board[0].setFill(color_rgb(*array[0]))
+        board[0].setOutline(color_rgb(*array[0]))
+        board[i].setFill(color_rgb(*array[i]))
+        board[i].setOutline(color_rgb(*array[i]))
+        # update heap
         heap.max_heapify(0, dim)
     print(array)
     return array
-    
-array = []
-for i in range(0, arr_width, 100):
-    for j in range(0, arr_width, 100):
+ 
+for i in range(0, row_size*square_width, square_width):
+    for j in range(0, row_size*square_width, square_width):
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         array.append(color)        
         p1 = Point(i, j)
-        p2 = Point(i+100, j+100)
+        p2 = Point(i+square_width, j+square_width)
         rect = Rectangle(p1, p2) 
         color = color_rgb(*color)
         print(color)
         rect.setFill(color)
         rect.setOutline(color)
         rect.draw(win)
-#while True:
-#    pass
+        board.append(rect)
+
 # heap
+#cProfile.run('heapsort(array, 2)') 
 print(heapsort(array, 2))
 print(heapsort(array, 1))
 print(heapsort(array, 0))
-#print(selection_sort(array,0))
-#print(selection_sort(array,2))
-#print(selection_sort(array,1))
 
-while True:
-    pass
+input("press enter to close")
 win.close()
-
-if __name__ == "__main__":
-    pass
